9b054e406abef74a705d589cedf919a2b982ff6c
Tirth <tirthacodes@gmail.com>
2023-06-23 11:00:49 +0545

    controller & module created + some minor changes

---
+++ src/students/students.controller.ts
@@ -0,0 +1,5 @@
+import { StudentService } from "./students.service";
+
+export class StudentsController{
+    studentService: StudentService;
+}
--- src/students/students.repository.ts
+++ src/students/students.repository.ts
@@ -1,15 +1,15 @@
 import { readFile,writeFile } from "fs/promises"; 
 
 export class StudentRepository{
-    async create(content:string){
+    async create(fullname: string, id: string, address: string, contact: string){
         const contents = await readFile('students.json', 'utf8');
 
         //converting raw data to json
         const students = JSON.parse(contents);
 
         //creating new students json
-        const id = Math.floor(Math.random() *999);
-        students[students.id] = {id:students.id, fullname:students.fullname, address:students.address, contact:students.contact};
+        
+        students[id] = {id:students.id, fullname:students.fullname, address:students.address, contact:students.contact};
         
         //store json
         await writeFile('students.json', JSON.stringify(students))
